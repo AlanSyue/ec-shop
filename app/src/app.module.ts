0e9a53fb8fec2114b8ae75050c15eccbf1cf9bef
alansyue <b123105@gmail.com>
2022-08-07 13:38:55 +0800

    feat: inject dependencies global

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -1,14 +1,26 @@
 import { AuthModule } from './auth/auth.module';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { config } from '../ormconfig';
+import { AppDataSource } from 'ormconfig'
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+import { ProductModule } from './product/product.module';
+import { CartModule } from './cart/cart.module';
+import { OrderModule } from './order/order.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(config),
+    TypeOrmModule.forRoot(AppDataSource.options),
     AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    UserModule,
+    ProductModule,
+    CartModule,
+    OrderModule,
   ],
   controllers: [],
   providers: [],
 })
-export class AppModule { }
+export class AppModule {}
